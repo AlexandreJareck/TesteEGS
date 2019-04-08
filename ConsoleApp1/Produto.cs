d705679a1141fbf5a15ac58720ca4d528edabbb5
Alexandre Jareck <alexandrejareck@gmail.com>
2019-04-08 20:11:31 -0300

    Teste EGS, concluido

---
+++ ConsoleApp1/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteEGS
+{
+    class Produto {
+
+        public string Nome;
+        public double Preco;
+        public double Desconto;
+        public double TotalDesconto;
+
+        public double DescontoProduto() {
+
+            Desconto = 0.6 * Preco;
+            return Desconto;
+
+        }
+
+        public double ValorComDesconto() {
+            TotalDesconto = Preco - Desconto;
+
+            return TotalDesconto;
+
+        }
+
+    }
+
+    
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace TesteEGS
+
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Produto produto = new Produto();
+
+            Console.WriteLine("Produtos com 60% de desconto");
+                                                    
+            Console.Write("Nome do produto: "); // porfavor, digite o nome do produto.
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço do produto: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Produto custava: " + produto.Preco.ToString("C", CultureInfo.CurrentCulture)); // testei assim 2834.53, 1999.51, 1002.55 ...
+            Console.WriteLine("Com Desconto: " + produto.DescontoProduto().ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine("Valor do desconto: " + produto.ValorComDesconto().ToString("C", CultureInfo.CurrentCulture));            
+
+
+        }
+    }
+}
